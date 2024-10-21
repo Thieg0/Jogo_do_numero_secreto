fba08c2c84134283968c9c0e5cdc25f18a164644
Thiego Macena <thiego1342@gmail.com>
2024-10-20 21:30:23 -0300

    Revert "Deixando o jogo mais facil"
    This reverts commit 114efda7d20a74cf76d7d396cd333d1d647c8ee4.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = "Escolha um número entre 1 e 10";*/
 
 let listaDeNumerosSorteados = [];
-let numeroLimite = 50;
+let numeroLimite = 100;
 let numeroSecreto = geraNumeroAleatorio();
 let tentativas = 1; 
 
